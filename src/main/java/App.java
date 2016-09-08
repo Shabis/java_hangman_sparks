3c9cb1699eff7fa1481941df6b3a7cd2b2183c54
Ewa Manek and Zachary Matthewstearn <zachary@>
2016-09-08 16:37:43 -0700

    Add difficulty levels, graphics, and phrase generation

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
 public class App {
   public static void main(String[] args) {
     Console console = System.console();
-    Hangman hangman = new Hangman();
+    Hangman hangman = null;
+    int difficulty = 0;
     String guess = "";
     int endCondition = 0;
 
@@ -12,10 +13,40 @@ public class App {
     System.out.println("Welcome to HAAAAANGMAAAAAAN!!!!one!!alsoOne!!!!!EXCLAMATIONpOINT!");
     System.out.println("One man enters, sometimes one man leaves. but also maybe no men leave.");
     System.out.println("(You know how Hangman works, right?)");
+    System.out.println("If not you should probably choose easy.");
+    System.out.println("Enter a number indicating your desired difficulty level, OR have a friend enter a word for you directly!\n1 - easy\n2 - medium\n3 - hard");
+    String userInput = new String(console.readPassword());
+    try {
+      difficulty = Integer.parseInt(userInput.trim());
+    } catch (NumberFormatException e){
+      hangman = new Hangman(userInput);
+    }
+    if(hangman == null){
+      if(difficulty < 1 || difficulty > 3){
+        System.out.println("Joke's on you, sucka!!!!!!");
+      }
+      hangman = new Hangman(difficulty);
+    }
     do{
       System.out.println("----------------------");
       System.out.println("You have " + (hangman.mMAX_COUNTER - hangman.getGameEndCounter()) + " guesses remaining to SAVE THIS RANDO'S LIFE!");
+      if(hangman.getGameEndCounter() > 0){
+        System.out.println("");
+        System.out.println(" O");
+      }
+      if(hangman.getGameEndCounter() > 3){
+        System.out.println("/|\\");
+      } else if(hangman.getGameEndCounter() > 2){
+        System.out.println("/|");
+      } else if (hangman.getGameEndCounter() > 1){
+        System.out.println(" |");
+      }
+      if(hangman.getGameEndCounter() > 4){
+        System.out.println("/");
+      }
+      System.out.println("");
       System.out.println("So far, this is what you know of the word: " + Arrays.toString(hangman.getProgress()));
+      System.out.println("Additionally, these be your guesses: " + hangman.getGuesses());
       System.out.println("Whatchu wanna guess???questionMark???");
       guess = String.valueOf(console.readLine().trim().toLowerCase().charAt(0));
       if(hangman.isGuessed(guess)){
@@ -33,11 +64,25 @@ public class App {
       }
       endCondition = hangman.endTurn();
     }while(endCondition == 0);
+    System.out.println("----------------------");
     if(endCondition == -1){
-      System.out.println("Oh nos! The rando is ded! You killed him. All you. No other factors were responsible for his death. You. The answer was " + hangman.getAnswer() + " by the way. A child could have guessed that.");
+      System.out.println("_______");
+      System.out.println("|  |   ");
+      System.out.println("|  O   ");
+      System.out.println("| /|\\  ");
+      System.out.println("| / \\  ");
+      System.out.println("|______");
+      System.out.println("");
+      System.out.println("Oh nos! The rando is ded! You killed him. All you. No other factors were responsible for his death. You. The answer was \"" + hangman.getAnswer() + "\" by the way. A child could have guessed that.");
     }
     else{
-      System.out.println("Congratulations! You successfully guessed that the word is " + hangman.getAnswer() + ". That must have been SOOOOOO hard. You are SOOOO smart and special. I guess the rando lives too. FOR NOW.");
+      System.out.println("");
+      System.out.println("\\O/");
+      System.out.println(" | ");
+      System.out.println("/ \\");
+      System.out.println("");
+      System.out.println("Congratulations! You successfully guessed that the answer is \"" + hangman.getAnswer() + "\". That must have been SOOOOOO hard. You are SOOOO smart and special. I guess the rando lives too. FOR NOW.");
     }
+    System.out.println("----------------------");
   }
 }
--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -2,32 +2,87 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Hangman{
   private String mAnswer = "";
-  private String[] mAnswerPool = {"hangman", "manswer", "cakemonger", "oxyphenbutazone", "hippocampus", "smorgasbord", "avuncular"};
+  private String[] mAnswerPool_Easy = {"hangman", "answer", "password", "random", "water"};
+  // private String[] mAnswerPool_Medium = {"cakemonger", "hippocampus", "smorgasbord", "lumberjack", "astronaut"};
+  // private String[] mAnswerPool_Hard = {"avuncular", "loquacious", "eschatological", "rhythm", "xenoanalysis"};
+  //private String[] mAnswerPool_Nouns = {"answer", "water", "hippocampus"};
+  Map<Integer, String[]> mAnswerPool_Nouns = new HashMap();
+  Map<Integer, String[]> mAnswerPool_Verbs = new HashMap();
+  String[] mAnswerPool_Prepositions = {"upon the", "excepting the", "save the", "within the", "via the"};
   private String[] mProgress;
   private int mGameEndCounter;
   public final int mMAX_COUNTER = 6;
   private List<String> mAlreadyGuessed= new ArrayList<String>();
 
-  public Hangman(){
-    Random random = new Random();
-    mAnswer = mAnswerPool[random.nextInt(mAnswerPool.length)];
-    mProgress = new String[mAnswer.length()];
-    for(int i = 0; i < mProgress.length; i++){
-      mProgress[i] = "_";
-    }
-    mGameEndCounter = 0;
-  }
 
   public Hangman(String answer){
     mAnswer = answer;
+    initializeProgress();
+    mGameEndCounter = 0;
+  }
+  public Hangman(){
+    Random random = new Random();
+    mAnswer = mAnswerPool_Easy[random.nextInt(mAnswerPool_Easy.length)];
+    initializeProgress();
+    mGameEndCounter = 0;
+  }
+  public Hangman(int difficulty){
+    mAnswerPool_Nouns.put(1, new String[] {"cakemonger", "hippocampus", "lumberjack", "aqueduct", "smorgasbord"});
+    mAnswerPool_Nouns.put(2, new String[] {"oncology", "genome", "cadenza", "block", "knife"});
+    mAnswerPool_Nouns.put(3, new String[] {"rhythm", "fox", "jazz", "quiz", "xylophone"});
+    mAnswerPool_Verbs.put(1, new String[] {"whisks", "leavens", "defenestrates", "deplanes", "devours"});
+    mAnswerPool_Verbs.put(2, new String[] {"coiffed", "plays", "hangs", "enters", "eats"});
+    mAnswerPool_Verbs.put(3, new String[] {"zapped", "will box", "is shivving", "was doffing", "shall razz"});
+    Random random = new Random();
+    switch(difficulty){
+      case 1:
+        mAnswer = mAnswerPool_Nouns.get(1)[random.nextInt(mAnswerPool_Nouns.get(1).length)];
+        mAnswer += " ";
+        mAnswer += mAnswerPool_Verbs.get(1)[random.nextInt(mAnswerPool_Nouns.get(1).length)];
+        break;
+      case 2:
+        mAnswer = mAnswerPool_Nouns.get(2)[random.nextInt(mAnswerPool_Nouns.get(2).length)];
+        mAnswer += " ";
+        mAnswer += mAnswerPool_Verbs.get(2)[random.nextInt(mAnswerPool_Verbs.get(2).length)];
+        break;
+      case 3:
+        mAnswer = mAnswerPool_Nouns.get(3)[random.nextInt(mAnswerPool_Nouns.get(3).length)];
+        mAnswer += " ";
+        mAnswer += mAnswerPool_Verbs.get(3)[random.nextInt(mAnswerPool_Verbs.get(3).length)];
+        mAnswer += " ";
+        mAnswer += mAnswerPool_Prepositions[random.nextInt(mAnswerPool_Prepositions.length)];
+        mAnswer += " ";
+        String possibility = "";
+        do{
+           possibility = mAnswerPool_Nouns.get(3)[random.nextInt(mAnswerPool_Nouns.get(3).length)];
+        }while(mAnswer.contains(possibility));
+        mAnswer += possibility;
+        break;
+      default:
+        mAnswer = "however little known the feelings or views of such a man may be on his first entering a neighbourhood this truth is so well fixed in the minds of the surrounding families that he is considered as the rightful property of some one or other of their daughters";
+        break;
+    }
+    initializeProgress();
+    mGameEndCounter = 0;
+  }
+
+  public void initializeProgress(){
     mProgress = new String[mAnswer.length()];
     for(int i = 0; i < mProgress.length; i++){
-      mProgress[i] = "_";
+      if(mAnswer.charAt(i) == ' '){
+        mProgress[i] = " ";
+      }
+      else {
+        mProgress[i] = "_";
+      }
     }
-    mGameEndCounter = 0;
   }
 
   public String getAnswer(){
@@ -36,6 +91,10 @@ public class Hangman{
 
   public String[] getProgress(){
     return mProgress;
+  }
+
+  public List<String> getGuesses(){
+    return mAlreadyGuessed;
   }
 
   public int getGameEndCounter(){
